c8b9d5c47cce8102510bf0c8a801473fc35cf2ea
Emil <60893397+Vorsteh@users.noreply.github.com>
2022-02-17 16:12:17 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,30 @@
+namespace aoc
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //-------DAY1-------
+            /* 
+            List<int> list = new List<int>();
+
+            list = File.ReadAllLines("C:\\Users\\Rwy\\source\\repos\\aoc\\aoc\\Day1.txt").Select(x =>
+            int.Parse(x)).ToList();
+
+            Day1 day1 = new Day1();
+            Console.WriteLine(day1.part1(list));
+            Console.WriteLine(day1.part2(list));
+            */
+
+            //-------DAY2--------
+            /*
+            List<string> list = new List<string>();
+            list = File.ReadAllLines("C:\\Users\\Rwy\\source\\repos\\aoc\\aoc\\Day2.txt").ToList();
+            Day2 day2 = new Day2();
+            Console.WriteLine("Part 1: " + day2.part1(list));
+            Console.WriteLine("Part 2: " + day2.part2(list));
+            */
+        }
+    }
+}
